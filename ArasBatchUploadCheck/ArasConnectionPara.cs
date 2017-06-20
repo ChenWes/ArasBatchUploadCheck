d5c191e43496a35e879292506c581eeb7dca6ac8
ChenWes <chenxuhua0530@163.com>
2017-06-20 11:37:23 +0800

    add old file

---
+++ ArasBatchUploadCheck/ArasConnectionPara.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArasBatchUploadCheck
+{
+    public class ArasConnectionPara
+    {
+        public string l_serverurl { get; set; }
+        public string l_db { get; set; }
+        public string l_username { get; set; }
+        public string l_password { get; set; }
+    }
+}
